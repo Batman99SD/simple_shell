4b99867d16226bff9f94776b41e28a635746cb05
Batman99SD <azozkrg1@gmail.com>
2023-11-15 09:50:34 -0800

    add main header

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(const char *s1, const char *s2);
+char **tok(char *command);
+void exec_wa(char *toks, char *const *argv, char **env);
+char *get_line(char **argv);
+int toknum(char *command);
+void _free(char **arr);
+char *_getenv(const char *name);
+void print_environment(void);
+int get_path(char **tokens, char **env);
+
+#endif
